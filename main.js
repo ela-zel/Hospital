0b07ba9b5cc73b1c0762ef7f1bb7979dbaa2b156
ela-zel <elazelfani771@gmail.com>
2021-12-29 13:21:08 +0100

    update

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to your hospital application." });
+  res.json({ message: "Welcome to your hospital application page." });
 });
 
 // set port, listen for requests
